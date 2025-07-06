2e4257a13775e34496057e7732f95b9268658f78
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-07-06 15:42:02 +0200

    Converted the morse app to use Click for the CLI

--- src/instrumentman/morse.py
+++ src/instrumentman/morse.py
@@ -1,72 +1,105 @@
-import argparse
 from time import sleep
+from typing import Callable, Any
+
+from click_extra import (
+    extra_command,
+    option,
+    option_group,
+    argument,
+    version_option,
+    Choice,
+    IntRange
+)
 
 from geocompy import open_serial, GeoCom
 
 
-def morse_beep(tps: GeoCom, args: argparse.Namespace) -> None:
-    def letter_beep(letter: str) -> None:
-        lookup = {
-            "a": ".-",
-            "b": "-...",
-            "c": "-.-.",
-            "d": "-..",
-            "e": ".",
-            "f": "..-.",
-            "g": "--.",
-            "h": "....",
-            "i": "..",
-            "j": ".---",
-            "k": "-.-",
-            "l": ".-..",
-            "m": "--",
-            "n": "-.",
-            "o": "---",
-            "p": ".--.",
-            "q": "--.-",
-            "r": ".-.",
-            "s": "...",
-            "t": "-",
-            "u": "..-",
-            "v": "...-",
-            "w": ".--",
-            "x": "-..-",
-            "y": "-.--",
-            "z": "--..",
-            "1": ".----",
-            "2": "..---",
-            "3": "...--",
-            "4": "....-",
-            "5": ".....",
-            "6": "-....",
-            "7": "--...",
-            "8": "---..",
-            "9": "----.",
-            "0": "-----"
-        }
-        code = lookup[letter]
-        for i, signal in enumerate(code):
-            beepstart(intensity)
-            match signal:
-                case ".":
-                    sleep(unit)
-                case "-":
-                    sleep(unit * 3)
-            beepstop()
-            if i != len(code) - 1:
+def morse_character(
+    letter: str,
+    unit: float,
+    beepstart: Callable[[], Any],
+    beepstop: Callable[[], Any]
+) -> None:
+    lookup = {
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--..",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        "0": "-----"
+    }
+    code = lookup[letter]
+    for i, signal in enumerate(code):
+        beepstart()
+        match signal:
+            case ".":
                 sleep(unit)
+            case "-":
+                sleep(unit * 3)
+        beepstop()
+        if i != len(code) - 1:
+            sleep(unit)
 
-    unit = args.unit * 1e-3
-    intensity = args.intensity
+
+def morse_message(
+    tps: GeoCom,
+    message: str,
+    intensity: int,
+    unit: float,
+    compatibility: str
+) -> None:
+    unit_seconds = unit * 1e-3
+
     beepstart = tps.bmm.beep_start
     beepstop = tps.bmm.beep_stop
-    if args.compatibility == "TPS1000":
-        beepstart = tps.bmm.beep_on
-        beepstop = tps.bmm.beep_off
-    words = args.message.lower().split(" ")
+    match compatibility.lower():
+        case "tps1000":
+            beepstart = tps.bmm.beep_on
+            beepstop = tps.bmm.beep_off
+        case "none":
+            pass
+        case _:
+            raise ValueError(f"Unknown compatibility option: {compatibility}")
+
+    words = message.lower().split(" ")
     for i, word in enumerate(words):
         for j, letter in enumerate(word):
-            letter_beep(letter)
+            morse_character(
+                letter,
+                unit_seconds,
+                lambda: beepstart(intensity),
+                beepstop
+            )
 
             if j != len(word) - 1:
                 sleep(unit * 3)
@@ -75,58 +108,80 @@ def morse_beep(tps: GeoCom, args: argparse.Namespace) -> None:
             sleep(unit * 7)
 
 
-def main(args: argparse.Namespace) -> None:
-    with open_serial(args.port, speed=args.baud, timeout=args.timeout) as com:
-        ts = GeoCom(com)
-        morse_beep(ts, args)
-
-
-def cli() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(
-        "morse",
-        description=(
-            "Play a Morse encoded ASCII message through the beep signals "
-            "of a GeoCom capable total station."
-        ),
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    group_com = parser.add_argument_group("communication")
-    group_com.add_argument("port", help="serial port", type=str)
-    group_com.add_argument(
+@extra_command(
+    "morse",
+    params=None,
+    context_settings={"auto_envvar_prefix": None}
+)  # type: ignore[misc]
+@version_option()
+@argument("port", type=str)
+@argument("message", type=str)
+@option(
+    "-i",
+    "--intensity",
+    help="beeping intensity",
+    type=IntRange(0, 100),
+    default=100
+)
+@option(
+    "-u",
+    "--unit",
+    help="beep unit time in milliseconds [ms]",
+    type=IntRange(min=50),
+    default=50
+)
+@option(
+    "-c",
+    "--compatibility",
+    help="instrument compatibility",
+    type=Choice(["none", "TPS1000"], case_sensitive=False),
+    default="none"
+)
+@option_group(
+    "Connection options",
+    "Options related to the serial connection",
+    option(
         "-b",
         "--baud",
-        help="speed",
+        help="serial speed",
         type=int,
         default=9600
-    )
-    group_com.add_argument(
+    ),
+    option(
         "-t",
         "--timeout",
-        help="timeout",
-        type=int,
+        help="serial timeout",
+        type=IntRange(min=0),
         default=15
     )
-    parser.add_argument(
-        "-u",
-        "--unit",
-        help="beep unit time in milliseconds",
-        type=int,
-        default=50
-    )
-    parser.add_argument(
-        "-c",
-        "--compatibility",
-        help="instrument compatibility mode",
-        type=str,
-        choices=["TPS1000"]
-    )
-    parser.add_argument("intensity", help="beep intensity [1-100]", type=int)
-    parser.add_argument("message", help="message to encode", type=str)
+)
+def cli(
+    port: str,
+    message: str,
+    intensity: int = 100,
+    baud: int = 9600,
+    timeout: int = 15,
+    unit: int = 50,
+    compatibility: str = "none",
+) -> None:
+    """Play a Morse encoded ASCII message through the beep signals
+        of a GeoCom capable total station.
 
-    return parser
+        PORT is the serial port, that the instrument is connected to. It
+        must be a valid identifier as a string (e.g. COM1 or /dev/usbtty0).
+
+        MESSAGE is the message to relay, as a string of ASCII characters.
+        """
+    with open_serial(port, speed=baud, timeout=timeout) as com:
+        ts = GeoCom(com)
+        morse_message(
+            ts,
+            message,
+            intensity,
+            unit,
+            compatibility
+        )
 
 
 if __name__ == "__main__":
-    parser = cli()
-    args = parser.parse_args()
-    main(args)
+    cli()
