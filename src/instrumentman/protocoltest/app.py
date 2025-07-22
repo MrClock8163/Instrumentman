3fb123d69ec2845cbb16c0bca3381dc8430073b9
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-07-22 14:55:08 +0200

    Overhauled GeoCom tests

--- src/instrumentman/protocoltest/app.py
+++ src/instrumentman/protocoltest/app.py
@@ -28,32 +28,119 @@ def tests_geocom(tps: GeoCom) -> None:
     )
     pause("Press any key when ready to proceed...")
 
-    echo("(Switching ATR off...)")
-    tps.aut.switch_atr(False)
-    tps.aus.switch_user_atr(False)
-    echo("(Switching to reflectorless EDM mode...)")
-    tps.bap.set_target_type('DIRECT')
-    resp_measure = tps.tmc.do_measurement()
-    resp_angles = tps.tmc.get_simple_measurement()
-    if (
-        resp_measure.error == GeoComCode.OK
-        and resp_angles.error == GeoComCode.OK
-    ):
-        echo_green("Measurements available")
+    echo("Testing subsystems...")
+    resp_aus = tps.aus.switch_user_atr(False)
+    if resp_aus.error == GeoComCode.OK:
+        echo_green("Alt User available")
     else:
-        echo_yellow(f"Measurements unavailable ({resp_measure.response})")
+        echo_yellow(f"Alt User unavailable ({resp_aus.response})")
 
-    resp_focus = tps.cam.set_focus_to_infinity()
-    if resp_focus.error == GeoComCode.OK:
+    tps.aut.switch_atr(False)
+    resp_aut = tps.aut.turn_to(0, Angle(180, 'deg'))
+    if resp_aut.error == GeoComCode.OK:
+        echo_green("Automation available")
+    else:
+        echo_yellow(f"Automation unavailable ({resp_aut.response})")
+
+    resp_bap = tps.bap.get_measurement_program()
+    if resp_bap.error == GeoComCode.OK:
+        echo_green("Basic Applications available")
+    else:
+        echo_yellow(f"Basic Applications unavailable ({resp_bap.response})")
+
+    resp_bmm = tps.bmm.beep_normal()
+    if resp_bmm.error == GeoComCode.OK:
+        echo_green("Basic Man-Machine interface available")
+    else:
+        echo_yellow(
+            "Basic Man-Machine interface unavailable "
+            f"({resp_bmm.response})"
+        )
+
+    resp_cam = tps.cam.set_focus_to_infinity()
+    if resp_cam.error == GeoComCode.OK:
+        echo_green("Camera available")
+    else:
+        echo_yellow(f"Camera unavailable ({resp_cam.response})")
+
+    echo_green("Communcation available")
+
+    resp_csv = tps.csv.get_instrument_name()
+    if resp_csv.error == GeoComCode.OK:
+        echo_green("Central Services available")
+    else:
+        echo_yellow(f"Central Services unavailable ({resp_csv.response})")
+
+    resp_ctl = tps.ctl.get_wakeup_counter()
+    if resp_ctl.error == GeoComCode.OK:
+        echo_green("Control Task available")
+    else:
+        echo_yellow(f"Control Task unavailable ({resp_ctl.response})")
+
+    resp_dna = tps.dna.switch_staffmode(False)
+    if resp_dna.error == GeoComCode.OK:
+        echo_green("Digital Level available")
+    else:
+        echo_yellow(f"Digital Level unavailable ({resp_dna.response})")
+
+    resp_edm = tps.edm.switch_laserpointer(False)
+    if resp_edm.error == GeoComCode.OK:
+        echo_green("Electronic Distance Measurement available")
+    else:
+        echo_yellow(
+            "Electronic Distance Measurement unavailable "
+            f"({resp_edm.response})"
+        )
+
+    resp_ftr = tps.ftr.setup_listing()
+    if resp_ftr.error == GeoComCode.OK:
+        echo_green("File Transfer available")
+        tps.ftr.abort_list()
+    else:
+        echo_yellow(f"File Transfer unavailable ({resp_ftr.response})")
+
+    resp_img = tps.img.get_telescopic_configuration()
+    if resp_img.error == GeoComCode.OK:
         echo_green("Imaging available")
     else:
-        echo_yellow(f"Imaging unavailable ({resp_focus.response})")
+        echo_yellow(f"Imaging unavailable ({resp_img.response})")
 
-    resp_changeface = tps.aut.turn_to(0, Angle(180, 'deg'))
-    if resp_changeface.error == GeoComCode.OK:
+    resp_kdm = tps.kdm.get_display_power_status()
+    if resp_kdm.error == GeoComCode.OK:
+        echo_green("Keyboard Display Unit available")
+    else:
+        echo_yellow(f"Keyboard Display Unit unavailable ({resp_kdm.response})")
+
+    tps.mot.stop_controller()
+    resp_mot = tps.mot.start_controller()
+    if resp_mot.error == GeoComCode.OK:
         echo_green("Motorization available")
     else:
-        echo_yellow(f"Mororization unavailable ({resp_changeface.response})")
+        echo_yellow(f"Motorization unavailable ({resp_mot.response})")
+
+    resp_sup = tps.sup.get_poweroff_configuration()
+    if resp_sup.error == GeoComCode.OK:
+        echo_green("Supervisor available")
+    else:
+        echo_yellow(f"Supervisor unavailable ({resp_sup.response})")
+
+    resp_tmc = tps.tmc.get_station()
+    if resp_tmc.error == GeoComCode.OK:
+        echo_green("Theodolite Measurement and Calculation available")
+    else:
+        echo_yellow(
+            "Theodolite Measurement and Calculation unavailable "
+            f"({resp_tmc.response})"
+        )
+
+    resp_wir = tps.wir.get_recording_format()
+    if resp_wir.error == GeoComCode.OK:
+        echo_green("Word Index Registration available")
+    else:
+        echo_yellow(
+            "Word Index Registration unavailable "
+            f"({resp_wir.response})"
+        )
 
 
 def tests_gsidna(dna: GsiOnlineDNA) -> None:
