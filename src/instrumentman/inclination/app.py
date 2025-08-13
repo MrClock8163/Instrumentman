783b273b3c8890ba652fdd4c2ec080e6a0e29f4d
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-08-13 22:19:10 +0200

    [incline] added logging to measurements

--- src/instrumentman/inclination/app.py
+++ src/instrumentman/inclination/app.py
@@ -2,6 +2,7 @@ from io import TextIOWrapper
 from time import sleep
 from math import tan, atan, degrees
 from re import compile
+from logging import Logger, getLogger
 
 from rich.console import Console
 from rich.progress import track
@@ -9,6 +10,7 @@ from rich.table import Table, Column
 from click_extra import echo
 from geocompy.data import Angle, Coordinate
 from geocompy.geo import GeoCom
+from geocompy.geo.gctypes import GeoComCode
 from geocompy.communication import open_serial
 
 from ..calculations import adjust_uniform_single
@@ -20,11 +22,13 @@ _LINE = compile(r"^\d+(?:\.\d+)?(?:,\-?\d+\.\d+){2}$")
 
 def run_measure(
     tps: GeoCom,
+    logger: Logger,
     output: TextIOWrapper | None = None,
     positions: int = 1,
     zero: bool = False,
     cycles: int = 1
 ) -> None:
+    logger.info("Starting inclination measurement")
     turn = 360 // positions
     v = Angle(90, 'deg')
     start = 0
@@ -33,7 +37,14 @@ def run_measure(
         angles = tps.tmc.get_angle()
         if angles.params is not None:
             start = round(angles.params[0].asunit('deg'))
+        else:
+            logger.error("Could not get current orientation, defaulting to 0")
 
+    logger.debug(
+        f"Measuring {cycles:d} cycles(s), {positions:d} position(s)/cycle "
+        f", {cycles * positions:d} position(s) total, "
+        f"starting at {start:d} degrees"
+    )
     con = Console()
     values: list[tuple[str, str, str]] = []
     for a in track(
@@ -41,12 +52,17 @@ def run_measure(
         description="Measuring",
         console=con
     ):
+        logger.info(f"Measuring at {a:d} degrees")
         hz = Angle(a, 'deg').normalized()
         tps.aut.turn_to(hz, v)
 
-        sleep(1)
+        sleep(1)  # giving time for the compensator to settle after the move
         fullangles = tps.tmc.get_angle_inclination('MEASURE')
-        if fullangles.params is None:
+        if fullangles.error != GeoComCode.OK or fullangles.params is None:
+            logger.error(
+                f"Could not measure inclination ({fullangles})"
+            )
+
             continue
 
         az = fullangles.params[0]
@@ -60,6 +76,8 @@ def run_measure(
                 f"{length.asunit('deg') * 3600:.2f}",
             )
         )
+
+    logger.info("Measurements complete")
 
     if output is not None:
         print(
@@ -94,6 +112,12 @@ def main_measure(
     zero: bool = False,
     cycles: int = 1
 ) -> None:
+    logger = getLogger("iman.inclination.measure")
+    logger.info(f"Opening connection on {port}")
+    logger.debug(
+        f"Connection parameters: baud={baud:d}, timeout={timeout:d}, "
+        f"tries={retry:d}, sync-after-timeout={str(sync_after_timeout)}"
+    )
     with open_serial(
         port,
         retry=retry,
@@ -101,8 +125,17 @@ def main_measure(
         speed=baud,
         timeout=timeout
     ) as com:
-        tps = GeoCom(com)
-        run_measure(tps, output, positions, zero, cycles)
+        tps = GeoCom(com, logger.getChild("instrument"))
+        run_measure(
+            tps,
+            logger,
+            output,
+            positions,
+            zero,
+            cycles
+        )
+
+    logger.info(f"Closed connection on {port}")
 
 
 def main_merge(
