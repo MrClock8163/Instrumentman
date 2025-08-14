3f4438b07663145bafd1dd4ee9fbe7c24ecc1df8
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-08-14 12:27:25 +0200

    [settings] added logging to upload

--- src/instrumentman/settings/load.py
+++ src/instrumentman/settings/load.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import Any, Callable
+from logging import Logger, getLogger
 
 from geocompy.communication import open_serial
 from geocompy.geo import GeoCom
@@ -16,6 +17,7 @@ from .validate import validate_settings
 
 
 def set_setting_geocom(
+    logger: Logger,
     system: GeoComSubsystem,
     setting: str,
     value: int | float | bool | str | list[int | float | bool | str]
@@ -31,6 +33,7 @@ def set_setting_geocom(
     ] | None = getattr(system, name, None)
     if method is None:
         echo_yellow(f"Could not find '{name}' to set '{setting}'")
+        logger.error(f"Could not find '{name}' to set '{setting}'")
         return
 
     if isinstance(value, list):
@@ -39,11 +42,13 @@ def set_setting_geocom(
         response = method(value)
 
     if response.error != GeoComCode.OK:
-        echo_yellow(f"Could not set '{setting}' ({response.error.name})")
+        echo_yellow(f"Could not set '{setting}'")
+        logger.error(f"Could not set '{setting}' ({response})")
         return
 
 
 def set_setting_gsidna(
+    logger: Logger,
     system: GsiOnlineDNASettings,
     setting: str,
     value: int | float | bool | str
@@ -55,49 +60,65 @@ def set_setting_gsidna(
     ] | None = getattr(system, name, None)
     if method is None:
         echo_yellow(f"Could not find '{name}' to set '{setting}'")
+        logger.error(f"Could not find '{name}' to set '{setting}'")
         return
 
     response = method(value)
 
     if response.value is None or not response.value:
-        echo_yellow(f"Could not set '{setting}' ({response.response})")
+        echo_yellow(f"Could not set '{setting}'")
+        logger.error(f"Could not set '{setting}' ({response.response})")
         return
 
 
 def upload_settings_geocom(
     protocol: GeoCom,
+    logger: Logger,
     settings: SettingsDict
 ) -> None:
+    logger.info("Starting GeoCom settings upload")
     for item in settings["settings"]:
         sysname = item["subsystem"]
         subsystem: Any = getattr(protocol, sysname)
         if subsystem is None:
             echo_red(f"Could not find '{sysname}' subsystem")
+            logger.critical(f"Could not find '{sysname}' subsystem")
             exit(1)
 
         for option, value in item["options"].items():
             if value is None:
+                logger.debug(f"Skipping {option}")
                 continue
 
-            set_setting_geocom(subsystem, option, value)
+            set_setting_geocom(logger, subsystem, option, value)
+
+    echo_green(f"Settings loaded from {settings}")
+    logger.info("Settings uploaded")
 
 
 def upload_settings_gsidna(
     protocol: GsiOnlineDNA,
+    logger: Logger,
     settings: SettingsDict
 ) -> None:
+    logger.info("Starting GSI Online DNA settings upload")
     for item in settings["settings"]:
         sysname = item["subsystem"]
         subsystem: Any = getattr(protocol, sysname)
         if subsystem is None:
             echo_red(f"Could not find '{sysname}' subsystem")
+            logger.critical(f"Could not find '{sysname}' subsystem")
             exit(1)
 
         for option, value in item["options"].items():
             if value is None:
+                logger.debug(f"Skipping {option}")
                 continue
 
-            set_setting_gsidna(subsystem, option, value)
+            set_setting_gsidna(logger, subsystem, option, value)
+
+    echo_green(f"Settings loaded from {settings}")
+    logger.info("Settings uploaded")
 
 
 def main(
@@ -109,11 +130,18 @@ def main(
     sync_after_timeout: bool = False,
     format: str = "auto"
 ) -> None:
+    logger = getLogger("iman.settings.load")
     data = read_settings(settings, format)
     if not validate_settings(data):
         echo_red("Settings file does not follow schema")
+        logger.critical("Settings file does not follow schema")
         exit(1)
 
+    logger.info(f"Opening connection on {port}")
+    logger.debug(
+        f"Connection parameters: baud={baud:d}, timeout={timeout:d}, "
+        f"tries={retry:d}, sync-after-timeout={str(sync_after_timeout)}"
+    )
     with open_serial(
         port,
         retry=retry,
@@ -123,13 +151,10 @@ def main(
     ) as com:
         match data["protocol"]:
             case "geocom":
-                tps = GeoCom(com)
-                upload_settings_geocom(tps, data)
+                tps = GeoCom(com, logger.getChild("instrument"))
+                upload_settings_geocom(tps, logger, data)
             case "gsidna":
-                dna = GsiOnlineDNA(com)
-                upload_settings_gsidna(dna, data)
-            case _:
-                echo_red(f"Unknown protocol: {data["protocol"]}")
-                exit(1)
+                dna = GsiOnlineDNA(com, logger.getChild("instrument"))
+                upload_settings_gsidna(dna, logger, data)
 
-        echo_green(f"Settings loaded from {settings}")
+    logger.info(f"Closed connection on {port}")
